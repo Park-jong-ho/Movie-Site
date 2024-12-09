6ceb2ec1b6628d953ff8a9e05aabf51d66e4cd2b
jongho <whjk1104@gmail.com>
2024-12-09 18:58:58 +0900

    ✨ Mission 2
    Mission 2

--- Movie-Site/src/component/MovieCard.jsx
+++ Movie-Site/src/component/MovieCard.jsx
@@ -1,18 +1,21 @@
+// MovieCard.jsx
 
 import { Link } from 'react-router-dom';
-
 
 // props 받기
 const MovieCard = ({ id, posterPath, title, voteAverage }) => {
   const imageUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
-  
+
   return (
     <div className="movie-card">
       {/* 상세페이지 이동 */}
-      <Link to={`/details/${id}`}> 
+      <Link to={`/details/${id}`} className="movie-link">
         <img src={imageUrl} alt={title} className="movie-poster" />
         <h3 className="movie-title">{title}</h3>
-        <p className="movie-rating"><span>★</span> {voteAverage}</p>
+        <p className="movie-rating">
+          {/* 소수점 두자리까지만 표시, 마지막 자리의 0은 제거 */}
+          <span>★</span> {parseFloat(voteAverage.toFixed(2))}
+        </p>
       </Link>
     </div>
   );
